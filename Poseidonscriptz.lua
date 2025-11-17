240a23f17609708dee33b238e430793b0c0f7448
TokenProfit <latifpsw@gmail.com>
2025-11-17 20:33:46 +0700

    Update URL in Poseidonscriptz.lua

--- Poseidonscriptz.lua
+++ Poseidonscriptz.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/d6e25b4cccd544f7aa6e03f983aa72f1"))()
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/dfefdff64fc24c5a96abdf267270fe2f"))()
